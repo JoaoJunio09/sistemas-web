fd5e6444014814d06c1cf8bca3388b5828c0f9a0
joao_junio <joaojunio818@gmail.com>
2025-02-27 22:50:31 -0300

    updates

---
+++ 27.02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 27/02 - Sistemas Web I</title>
+</head>
+<body>
+    <?php
+        $valor = 10;
+        $valor2 = 10.33;
+        $nome = "João Junio";
+        $checado = true;
+        
+        echo "Exemplo de um programa em PHP";
+        echo "<P>";
+
+        echo "Conteúdo do valor: ";
+        echo $valor;
+        echo "<P>";
+    ?>
+</body>
+</html>
